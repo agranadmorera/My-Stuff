bc00a90cf6f585a531cf37d2a05e4ee83e63ec90
agranadmorera <97134930+agranadmorera@users.noreply.github.com>
2022-02-16 21:44:48 -0600

    Add files via upload

---
+++ Intercambio de Variables - Andres Granados.py
@@ -0,0 +1,17 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+x = 7;
+
+y = 5;
+
+aux = x;
+
+x = y;
+
+y = aux;
+
+print(x)
+5
+print(y)
+7
